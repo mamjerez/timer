62af94da03fc57b3296efb15df3da8855568797f
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-29 14:53:40 +0200

    Finish lesson 13

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,11 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-display',
   templateUrl: 'display.component.html',
   styleUrls: ['./display.component.scss'],
 })
-export class DisplayComponent {
-  @Input() time: number = 0;
+export class DisplayComponent implements OnChanges {
+  @Input() time: any = 65;
+  public minutes: string = '00';
+  public seconds: string = '00';
+
   constructor() {}
+
+  ngOnChanges(changes: any) {
+    if (changes.time) {
+      const minutes = Math.trunc(changes.time.currentValue / 60);
+      const seconds = changes.time.currentValue - minutes * 60;
+
+      this.minutes = ('0' + minutes).substr(-2);
+      this.seconds = ('0' + seconds).substr(-2);
+    }
+  }
 }
